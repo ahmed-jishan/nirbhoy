eb9433e4591bdecd602266ab6cbbb63c04c7ae51
ahmed-jishan <iamjishanahmed@gmail.com>
2026-07-16 17:56:36 +0600

    refactor: remove Sentry integration and related configurations
    - Deleted Sentry client and server configuration files to remove Sentry integration.
    - Removed Sentry dependency from package.json.
    - Deleted TypeScript configuration file.

--- next.config.js
+++ next.config.js
@@ -3,18 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-// Wrap with Sentry
-const { withSentryConfig } = require("@sentry/nextjs");
-module.exports = withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG || "nirbhoy",
-  project: process.env.SENTRY_PROJECT || "nirbhoy",
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  hideSourceMaps: true,
-  webpack: {
-    treeshake: {
-      removeDebugLogging: true,
-    },
-    automaticVercelMonitors: true,
-  },
-});
+module.exports = nextConfig;
